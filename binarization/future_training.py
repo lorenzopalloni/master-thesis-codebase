4149df448405df841bb7e18ea6f630ccfb305960
lorenzopalloni <palloni.lorenzo@gmail.com>
2022-08-17 08:32:40 +0200

    WIP: binarization/future_training.py

--- binarization/future_training.py
+++ binarization/future_training.py
@@ -1,5 +1,6 @@
+"""Training module"""
+
 from pathlib import Path
-from typing import Optional
 
 import hydra
 import lpips
@@ -12,6 +13,7 @@ from omegaconf import DictConfig
 from tqdm import tqdm
 
 from binarization import dataset, models, utils
+from binarization.vaccaro import pytorch_ssim
 
 cs = hydra.core.config_store.ConfigStore()
 cs.store(name="config_schema", node=MainConfig)
@@ -38,21 +40,22 @@ def main(cfg: DictConfig):
 
     dis = models.Discriminator()
 
-    # dis_optim = torch.optim.Adam(lr=cfg.params.dis_lr, params=dis.parameters())
-    # gen_optim = torch.optim.Adam(lr=cfg.params.gen_lr, params=gen.parameters())
+    dis_optim = torch.optim.Adam(lr=cfg.params.dis_lr, params=dis.parameters())
+    gen_optim = torch.optim.Adam(lr=cfg.params.gen_lr, params=gen.parameters())
 
     # ssim_loss_op = piq.SSIMLoss()
-    # lpips_vgg_loss_op = lpips.LPIPS(net='vgg', version='0.1')
-    # lpips_alex_loss_op = lpips.LPIPS(net='alex', version='0.1')
-    # bce_loss_op = torch.nn.BCELoss()
+    ssim_loss_op = pytorch_ssim.SSIM()
+    lpips_vgg_loss_op = lpips.LPIPS(net='vgg', version='0.1')
+    lpips_alex_loss_op = lpips.LPIPS(net='alex', version='0.1')
+    bce_loss_op = torch.nn.BCELoss()
 
-    # # Move models and losses to cuda device if any is available
-    # device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    # gen.to(device)
-    # dis.to(device)
-    # lpips_vgg_loss_op.to(device)
-    # lpips_alex_loss_op.to(device)
-    # print(f'current device: {device}')
+    # Move models and losses to cuda device if any is available
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    gen.to(device)
+    dis.to(device)
+    lpips_vgg_loss_op.to(device)
+    lpips_alex_loss_op.to(device)
+    print(f'current device: {device}')
 
     dl_train = dataset.make_train_dataloader(cfg)
     dl_val = dataset.make_val_dataloader(cfg)
@@ -63,7 +66,6 @@ def main(cfg: DictConfig):
         progress_bar_train = tqdm(
             dl_train, total=cfg.params.limit_train_batches
         )
-        # TODO: refactor lq -> compressed_patches, hq -> original_patches
         for step_id_train, (compressed_patches, original_patches) in enumerate(
             progress_bar_train
         ):
@@ -75,82 +77,96 @@ def main(cfg: DictConfig):
             gen.train()
             # dis.train()
 
-            # # Discriminator training step
-            # ##################################################################
-            # dis_optim.zero_grad()
+            # Discriminator training step
+            ##################################################################
+            dis_optim.zero_grad()
 
-            # lq = lq.to(device)
-            # hq = hq.to(device)
-            # generated_hq = gen(lq)
+            # both added on __getitem__
+            compressed_patches = compressed_patches.to(device)
+            original_patches = original_patches.to(device)
+            generated_original_patches = gen(compressed_patches)
 
-            # pred_hq = dis(hq)
+            pred_original_patches = dis(original_patches)
 
-            # loss_true = bce_loss_op(pred_hq, torch.ones_like(pred_hq))
+            loss_true = bce_loss_op(
+                pred_original_patches, torch.ones_like(pred_original_patches)
+            )
 
-            # pred_generated_hq = dis(generated_hq.detach())
-            # loss_fake = bce_loss_op(
-            #     pred_generated_hq, torch.zeros_like(pred_generated_hq)
+            pred_generated_original_patches = dis(
+                generated_original_patches.detach()
+            )
+            loss_fake = bce_loss_op(
+                pred_generated_original_patches,
+                torch.zeros_like(pred_generated_original_patches),
+            )
+
+            loss_dis = loss_true + loss_fake
+            loss_dis *= 0.5
+
+            loss_dis.backward()
+            dis_optim.step()
+            ##################################################################
+
+            # generator training step
+            ##################################################################
+            gen_optim.zero_grad()
+
+            loss_lpips = lpips_vgg_loss_op(
+                generated_original_patches, original_patches
+            ).mean()
+
+            # x_min = min(
+            #     generated_original_patches.min(), original_patches.min()
             # )
-
-            # loss_dis = loss_true + loss_fake
-            # loss_dis *= 0.5
-
-            # loss_dis.backward()
-            # dis_optim.step()
-            # ##################################################################
-
-            # # generator training step
-            # ##################################################################
-            # gen_optim.zero_grad()
-
-            # loss_lpips = lpips_vgg_loss_op(generated_hq, hq).mean()
-
-            # x_min = min(generated_hq.min(), hq.min())
-            # x_max = max(generated_hq.max(), hq.max())
+            # x_max = max(
+            #     generated_original_patches.max(), original_patches.max()
+            # )
             # loss_ssim = 1.0 - ssim_loss_op(
-            #     dataset.min_max_scaler(generated_hq, x_min, x_max),
-            #     dataset.min_max_scaler(hq, x_min, x_max),
+            #     dataset.min_max_scaler(
+            #         generated_original_patches, x_min, x_max
+            #     ),
+            #     dataset.min_max_scaler(original_patches, x_min, x_max),
             # )
 
-            # pred_generated_hq = dis(generated_hq)
-            # loss_bce = bce_loss_op(
-            #     pred_generated_hq, torch.ones_like(pred_generated_hq)
-            # )
-            # loss_gen = (
-            #     cfg.params.w0 * loss_lpips
-            #     + cfg.params.w1 * loss_ssim
-            #     + cfg.params.w2 * loss_bce
-            # )
+            loss_ssim = 1.0 - ssim_loss_op(
+                generated_original_patches, original_patches
+            )
 
-            # loss_gen.backward()
-            # gen_optim.step()
-            # ##################################################################
+            pred_generated_original_patches = dis(generated_original_patches)
+            loss_bce = bce_loss_op(
+                pred_generated_original_patches,
+                torch.ones_like(pred_generated_original_patches),
+            )
+            loss_gen = (
+                cfg.params.w0 * loss_lpips
+                + cfg.params.w1 * loss_ssim
+                + cfg.params.w2 * loss_bce
+            )
 
-            # # logging statistics on training set
-            # ##################################################################
-            # progress_bar.set_description(
-            #     'Epoch #{} - '
-            #     'Loss dis: {:.8f}; '
-            #     'Loss gen: {:.4f} = '
-            #     '({:.4f} + {:.4f} * {:.4f})'.format(
-            #         epoch_id,
-            #         float(loss_dis),
-            #         float(loss_gen),
-            #         float(
-            #             cfg.params.w0 * loss_lpips + cfg.params.w1 * loss_ssim
-            #         ),
-            #         float(cfg.params.w2),
-            #         float(loss_bce),
-            #     )
-            # )
-            # ##################################################################
-            # tensorboard_logger.add_scalar(
-            #     'lossD', scalar_value=loss_dis, global_step=global_step_id
-            # )
-            # tensorboard_logger.add_scalar(
-            #     'lossG', scalar_value=loss_gen, global_step=global_step_id
-            # )
-            # # tensorboard_logger.add_image('output_example', img_tensor=<insert-image-here>, global_step=epoch_id * step_id)
+            loss_gen.backward()
+            gen_optim.step()
+            ##################################################################
+
+            # logging statistics on training set
+            ##################################################################
+            progress_bar_train.set_description(
+                f'Epoch #{epoch_id} - '
+                f'Loss dis: {float(loss_dis):.8f}; '
+                f'Loss gen: {float(loss_gen):.4f} = '
+                # f'{float(cfg.params.w0 * loss_lpips + cfg.params.w1 * loss_ssim):.4f}'
+                f'{float(cfg.params.w0 * loss_lpips):.4f}'
+                f' + {cfg.params.w2:.4f}'
+                f' * {float(loss_bce):.4f})'
+            )
+            ##################################################################
+            tensorboard_logger.add_scalar(
+                'lossD', scalar_value=loss_dis, global_step=global_step_id
+            )
+            tensorboard_logger.add_scalar(
+                'lossG', scalar_value=loss_gen, global_step=global_step_id
+            )
+            # tensorboard_logger.add_image('output_example', img_tensor=<insert-image-here>, global_step=epoch_id * step_id)
+
             global_step_id += 1
 
         progress_bar_val = tqdm(dl_val, total=cfg.params.limit_val_batches)
@@ -169,33 +185,22 @@ def main(cfg: DictConfig):
                 compressed_patch_val = compressed_patches_val[batch_id_val]
                 original_patch_val = original_patches_val[batch_id_val]
 
-                _, original_width, original_height = original_patch_val.shape
-                print(
-                    f'original_height: {original_height}, original_width: {original_width}'
-                )
-                (
-                    _,
-                    compressed_width,
-                    compressed_height,
-                ) = compressed_patch_val.shape
-                print(
-                    f'encoded_height: {compressed_width}, encoded_width: {compressed_height}'
-                )
-
                 # preprocessed_compressed_patch_val = F.resize(
                 #     compressed_patch_val,
                 #     size=(256, original_width // cfg.params.unet.scale_factor)
                 # )
 
                 preprocessed_compressed_patch_val = (
-                    dataset.adjust_image_for_unet(compressed_patch_val)
+                    (dataset.adjust_image_for_unet(compressed_patch_val))
+                    .unsqueeze(0)
+                    .to(device)
                 )
 
                 gen.eval()
-
-                generated_patch_val = gen(
-                    preprocessed_compressed_patch_val.unsqueeze(0)
-                )
+                with torch.no_grad():
+                    generated_patch_val = gen(
+                        preprocessed_compressed_patch_val
+                    )
 
                 generated_patch_val_pil = F.to_pil_image(
                     dataset.inv_min_max_scaler(generated_patch_val)
@@ -204,12 +209,8 @@ def main(cfg: DictConfig):
                     / 255.0
                 )
 
-                original_patch_val_pil = F.to_pil_image(
-                    dataset.inv_min_max_scaler(original_patch_val)
-                )
-                compressed_patch_val_pil = F.to_pil_image(
-                    dataset.inv_min_max_scaler(compressed_patch_val)
-                )
+                original_patch_val_pil = F.to_pil_image(original_patch_val)
+                compressed_patch_val_pil = F.to_pil_image(compressed_patch_val)
 
                 fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(
                     2, 2, figsize=(30, 15)
@@ -224,7 +225,7 @@ def main(cfg: DictConfig):
                 ax4.set_title('reconstructed')
 
                 tensorboard_logger.add_figure(
-                    f"epoch{epoch_id:03d}-step_id_val-{step_id_val:03d}",
+                    f"epoch-{epoch_id:03d}-step_id_val-{step_id_val:03d}",
                     figure=fig,
                     global_step=global_step_id,
                 )
@@ -241,4 +242,4 @@ def main(cfg: DictConfig):
 
 
 if __name__ == "__main__":
-    main()
+    main()  # pylint: disable=no-value-for-parameter
