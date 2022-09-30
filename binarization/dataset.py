0dd96eb6bfacb5ac47f8038ae5597ad093b954b6
lorenzopalloni <palloni.lorenzo@gmail.com>
2022-09-30 07:37:50 +0200

    Adjust the custom torch.Dataset design

--- binarization/dataset.py
+++ binarization/dataset.py
@@ -1,7 +1,12 @@
+"""Data module"""
+
 import functools
 import itertools
+import warnings
+import json
+from enum import Enum
 from pathlib import Path
-from typing import List, Tuple
+from typing import Any, Callable, Dict, List, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,8 +14,15 @@ import PIL
 import torch
 import torchvision.transforms.functional as F
 from gifnoc import Gifnoc
+from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
 from torchvision.utils import Image
+
+
+class Stage(Enum):
+    TRAIN = 'train'
+    VAL = 'val'
+    TEST = 'test'
 
 
 def compute_adjusted_dimension(an_integer: int) -> int:
@@ -19,7 +31,7 @@ def compute_adjusted_dimension(an_integer: int) -> int:
     - is divisible at least four times by 2
     - is the closest to `an_integer`
 
-    Useful for adapting the size of an image to feed a UNet-like architecture.
+    Adapts image sizes to feed a UNet-like architecture.
 
     Args:
         an_integer (int): an integer greater than 0.
@@ -38,7 +50,7 @@ def compute_adjusted_dimension(an_integer: int) -> int:
 
 
 def adjust_image_for_unet(image: torch.Tensor) -> torch.Tensor:
-    """Pad until image height and width are divisible by 2 at least 4 times"""
+    """Pads until img_h and img_w are both divisible by 2 at least 4 times."""
     height, width = image.shape[-2], image.shape[-1]
     adjusted_height = compute_adjusted_dimension(height)
     adjusted_width = compute_adjusted_dimension(width)
@@ -57,9 +69,7 @@ def draw_validation_fig(
     generated_image: torch.Tensor,
     figsize: Tuple[int, int] = (12, 5),
 ) -> plt.Figure:
-    """Draw a matplotlib figure useful for validating the training process
-    of a representation model.
-    """
+    """Draws three images in a row with matplotlib."""
     original_image_pil = F.to_pil_image(original_image)
     compressed_image_pil = F.to_pil_image(compressed_image)
     generated_image_pil = F.to_pil_image(generated_image)
@@ -95,11 +105,11 @@ def get_starting_random_position(
     return random_position
 
 
-def crop_patches(
-    compressed_image: PIL.Image.Image,
+def random_crop_images(
     original_image: PIL.Image.Image,
-    patch_size: int,
-    scale_factor: int,
+    compressed_image: PIL.Image.Image,
+    patch_size: int = 96,
+    scale_factor: int = 2,
 ) -> Tuple[PIL.Image.Image, PIL.Image.Image]:
     """Randomly crops two images.
 
@@ -122,9 +132,9 @@ def crop_patches(
     original_image_positions = tuple(
         map(lambda x: x * scale_factor, compressed_image_positions)
     )
-    compressed_patch = compressed_image.crop(compressed_image_positions)
     original_patch = original_image.crop(original_image_positions)
-    return compressed_patch, original_patch
+    compressed_patch = compressed_image.crop(compressed_image_positions)
+    return original_patch, compressed_patch
 
 
 def lists_have_same_elements(a_list: List, another_list: List) -> bool:
@@ -137,35 +147,41 @@ def lists_have_same_elements(a_list: List, another_list: List) -> bool:
 
 
 def list_files(
-    path: Path, extension: str = '.jpg', sort_result: bool = True
+    path: Path, extension: str, sort_ascending: bool = True
 ) -> List[Path]:
     """List files in a given directory with the same extension.
 
     By default, the result is provided in lexicographic order.
     """
-    res = [
-        x
-        for x in path.iterdir()
-        if not x.is_dir() and x.name.endswith(extension)
-    ]
-    if sort_result:
+    res = []
+    for x in path.iterdir():
+        if not x.is_dir():
+            if x.suffix.lstrip('.') == extension.lstrip('.'):
+                res.append(x)
+            else:
+                warnings.warn(
+                    f'{x} has not been included since it has '
+                    f'a different extension than {extension}.',
+                    UserWarning,
+                )
+    if sort_ascending:
         return sorted(res)
     return res
 
 
-def list_directories(path: Path, sort_result: bool = True) -> List[Path]:
+def list_directories(path: Path, sort_ascending: bool = True) -> List[Path]:
     """List all the directories in a given path.
 
     By default, the result is provided in lexicographic order.
     """
     res = [x for x in path.iterdir() if x.is_dir()]
-    if sort_result:
+    if sort_ascending:
         return sorted(res)
     return res
 
 
-def list_files_in_sub_dirs(
-    path: Path, extension: str = '.jpg', sort_result: bool = True
+def list_subdir_files(
+    path: Path, extension: str, sort_ascending: bool = True
 ) -> List[Path]:
     """List all files in the second level directories of the given path.
 
@@ -173,11 +189,11 @@ def list_files_in_sub_dirs(
     """
     res = itertools.chain.from_iterable(
         (
-            list_files(i_dir, extension, sort_result=False)
+            list_files(i_dir, extension, sort_ascending=False)
             for i_dir in list_directories(path)
         )
     )
-    if sort_result:
+    if sort_ascending:
         return sorted(res)
     return list(res)
 
@@ -192,125 +208,257 @@ def min_max_scaler(
 def inv_min_max_scaler(
     tensor: torch.Tensor, tensor_min: float = 0.0, tensor_max: float = 255.0
 ) -> torch.Tensor:
-    """Inverse of the min_max_scaler function"""
+    """Inverse of the min_max_scaler function."""
     return (tensor * (tensor_max - tensor_min) + tensor_min).int()
 
 
-class WholeImagesDataset(Dataset):
-    def __init__(
-        self,
-        original_frames_dir: Path,
-        encoded_frames_dir: Path,
-        training: bool = False,
-        scale_factor: int = 2,
-    ):
-        """Custom PyTorch Dataset loader for the training phase. Yield a pair
-        (x, y), where x is the encoded version of the original image y.
+def default_test_pipe(
+    original_filepath: Path,
+    compressed_filepath: Path,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    original_image = Image.open(original_filepath)
+    compressed_image = Image.open(compressed_filepath)
 
-        Args:
-            original_frames_dir (Union[Path, str]): Original frames directory.
-            encoded_frames_dir (Union[Path, str]): Encoded frames directory.
-            training (bool, optional): Flag for training vs evaluation phase.
-                Defaults to False.
-            scale_factor (Optional[int]): Scale factor between original and
-                encoded frames. Defaults to 2, this means that original frames
-                have a 2:1 resolution ratio compared to encoded frames.
-        """
-        self.training = training
-        self.original_filenames = list_files_in_sub_dirs(
-            Path(original_frames_dir)
-        )
-        self.encoded_filenames = list_files_in_sub_dirs(
-            Path(encoded_frames_dir)
-        )
-        self.num_examples = len(self.original_filenames)
-        self.scale_factor = scale_factor
-
-    def __len__(self):
-        return self.num_examples
-
-    def __getitem__(self, i):
-
-        compressed_image = Image.open(self.encoded_filenames[i])
-        original_image = Image.open(self.original_filenames[i])
-
-        if self.training:
-            if np.random.random() < 0.5:
-                compressed_image = F.hflip(compressed_image)
-                original_image = F.hflip(original_image)
-
-        return (
-            min_max_scaler(F.pil_to_tensor(compressed_image)),
-            min_max_scaler(F.pil_to_tensor(original_image)),
-        )
-
-
-class CustomPyTorchDataset(Dataset):
-    def __init__(
-        self,
-        original_frames_dir: Path,
-        encoded_frames_dir: Path,
-        patch_size: int = 96,
-        training: bool = True,
-        scale_factor: int = 2,
-    ):
-        """Custom PyTorch Dataset loader for the training phase. Yield a pair
-        (x, y), where x is the encoded version of the original image y.
-
-        Args:
-            original_frames_dir (Union[Path, str]): Original frames directory.
-            encoded_frames_dir (Union[Path, str]): Encoded frames directory.
-            patch_size (int): Width/height of a training patch.
-                A training patch will be choosen at random from a given frame.
-            training (bool, optional): Flag for training vs evaluation phase.
-                Defaults to True.
-            scale_factor (Optional[int]): Scale factor between original and
-                encoded frames. Defaults to 2, this means that original frames
-                have a 2:1 resolution ratio compared to encoded frames.
-        """
-        self.patch_size = patch_size
-        self.training = training
-        self.original_filenames = list_files_in_sub_dirs(
-            Path(original_frames_dir)
-        )
-        self.encoded_filenames = list_files_in_sub_dirs(
-            Path(encoded_frames_dir)
-        )
-        self.num_examples = len(self.original_filenames)
-        self.scale_factor = scale_factor
-
-    def __len__(self):
-        return self.num_examples
-
-    def __getitem__(self, i):
-
-        compressed_image = Image.open(self.encoded_filenames[i])
-        original_image = Image.open(self.original_filenames[i])
-
-        compressed_patch, original_patch = crop_patches(
-            compressed_image=compressed_image,
-            original_image=original_image,
-            patch_size=self.patch_size,
-            scale_factor=self.scale_factor,
-        )
-
-        if np.random.random() < 0.5:
-            compressed_patch = F.hflip(compressed_patch)
-            original_patch = F.hflip(original_patch)
-
-        return (
-            min_max_scaler(F.pil_to_tensor(compressed_patch)),
-            min_max_scaler(F.pil_to_tensor(original_patch)),
-        )
-
-
-def make_train_dataloader(cfg: Gifnoc) -> DataLoader:
-    dataset = CustomPyTorchDataset(
-        original_frames_dir=cfg.paths.train_original_frames_dir,
-        encoded_frames_dir=cfg.paths.train_encoded_frames_dir,
-        patch_size=cfg.params.patch_size,
-        training=True,
+    return (
+        min_max_scaler(F.pil_to_tensor(original_image)),
+        min_max_scaler(F.pil_to_tensor(compressed_image)),
     )
+
+
+def default_val_pipe(
+    original_filepath: Path,
+    compressed_filepath: Path,
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    original_image = Image.open(original_filepath)
+    compressed_image = Image.open(compressed_filepath)
+
+    original_patch, compressed_patch = random_crop_images(
+        original_image=original_image,
+        compressed_image=compressed_image,
+    )
+
+    return (
+        min_max_scaler(F.pil_to_tensor(original_patch)),
+        min_max_scaler(F.pil_to_tensor(compressed_patch)),
+    )
+
+
+def default_train_pipe(
+    original_filepath: Path, compressed_filepath: Path
+) -> Tuple[torch.Tensor, torch.Tensor]:
+    original_image = Image.open(original_filepath)
+    compressed_image = Image.open(compressed_filepath)
+
+    original_patch, compressed_patch = random_crop_images(
+        original_image=original_image,
+        compressed_image=compressed_image,
+    )
+
+    if np.random.random() < 0.5:
+        original_patch = F.hflip(original_patch)
+        compressed_patch = F.hflip(compressed_patch)
+
+    return (
+        min_max_scaler(F.pil_to_tensor(original_patch)),
+        min_max_scaler(F.pil_to_tensor(compressed_patch)),
+    )
+
+
+def identity_pipe(original_filepath: Path, compressed_filepath: Path):
+    return original_filepath, compressed_filepath
+
+
+class ImageFilepathDataset(Dataset):
+    def __init__(
+        self,
+        original_filepaths: List[Path],
+        compressed_filepaths: List[Path],
+        pipe: Callable = identity_pipe,
+    ):
+        self.original_filepaths = original_filepaths
+        self.compressed_filepaths = compressed_filepaths
+        self.pipe = pipe
+
+    def __len__(self) -> int:
+        _len = len(self.original_filepaths)
+        assert len(self.compressed_filepaths) == _len
+        return _len
+
+    def __getitem__(self, i):
+        original_fn, compressed_fn = (
+            self.original_filepaths[i],
+            self.compressed_filepaths[i],
+        )
+        return self.pipe(original_fn, compressed_fn)
+
+
+def get_train_val_test_indexes(
+    n: int,
+    val_ratio: float = 0.025,
+    test_ratio: float = 0.025,
+    random_state: int = 42,
+) -> Tuple[List[int], List[int], List[int]]:
+    """Returns a partition of three arrays of random indexes.
+
+    >>> a, b, c = get_train_val_test_indexes(800)
+    >>> len(a), len(b), len(c)
+    (760, 20, 20)
+
+    Args:
+        n (int): Number of indexes that will rane from `0` to `n - 1`.
+        val_ratio (float, optional): Percentage of integers to
+            allocate for the validation set, between `0.0` and `1.0`.
+            Defaults to 0.025.
+        test_ratio (float, optional): Percentage of integers to
+            allocate for the test set, between `0.0` and `1.0`.
+            Defaults to 0.025.
+        random_state (int, optional): Random seed for replicability.
+            Defaults to 42.
+
+    Returns:
+        Tuple[List[int], List[int], List[int]]: Partition of three list of
+            integers choosen at random that range from `0` to `n - 1`,
+            without replacement.
+    """
+    random_state = 42
+    train_indexes, val_test_indexes = train_test_split(
+        range(n), test_size=val_ratio + test_ratio, random_state=random_state
+    )
+    val_indexes, test_indexes = train_test_split(
+        val_test_indexes,
+        test_size=test_ratio / (val_ratio + test_ratio),
+        random_state=random_state,
+    )
+    return train_indexes, val_indexes, test_indexes
+
+
+def make_splits(
+    original_frames_dir: Path,
+    compressed_frames_dir: Path,
+    save_path: Optional[Path] = None,
+    val_ratio: float = 0.025,
+    test_ratio: float = 0.025,
+    random_state: int = 42,
+) -> Dict[str, List[str]]:
+    """Makes train, val, test splits.
+
+    It needs two directory paths, one of original frames and
+    another one for the compressed versions. It checks first
+    that the two folders have the same folder names as children.
+
+    Args:
+        original_frames_dir (Path): Directory containing folders
+            for all original videos that contain their frame each.
+        compressed_frames_dir (Path): Directory containing folders
+            for all compressed videos that contain their frame each.
+        save_path (Optional[Path], optional): Filepath with ext .json
+            to save the resulting dictionary. Defaults to None.
+
+    Returns:
+        Dict[str, List[str]]: Partition in lists of all the filepaths
+            found in `original_frames_dir`, of the form
+            {'train': [], 'val': [], 'test': []}.
+    """
+    o_subdirs = list_directories(original_frames_dir, sort_ascending=True)
+    c_subdirs = list_directories(compressed_frames_dir, sort_ascending=True)
+    assert len(o_subdirs) == len(c_subdirs) and all(
+        x.name == y.name for x, y in zip(o_subdirs, c_subdirs)
+    )
+    n_videos = len(o_subdirs)
+    train_indexes, val_indexes, test_indexes = get_train_val_test_indexes(
+        n=n_videos,
+        val_ratio=val_ratio,
+        test_ratio=test_ratio,
+        random_state=random_state,
+    )
+    splits = {
+        'train': [o_subdirs[idx].name for idx in train_indexes],
+        'val': [o_subdirs[idx].name for idx in val_indexes],
+        'test': [o_subdirs[idx].name for idx in test_indexes],
+    }
+    if save_path is not None:
+        with open(save_path, 'w', encoding='utf-8') as out_file:
+            json.dump(splits, out_file)
+    return splits
+
+
+def get_splits(cfg: Gifnoc) -> Dict[str, List[str]]:
+    """Fetches `data_dir/splits.json`.
+
+    If it does not exist, it creates it, then returns it.
+
+    Args:
+        cfg (Gifnoc): Configuration object.
+
+    Returns:
+        Dict[str, List[str]]: Partition in lists of all the filepaths
+            found in `original_frames_dir`, of the form
+            {'train': [], 'val': [], 'test': []}.
+    """
+    splits_fp = Path(cfg.paths.data_dir, 'splits.json')
+    if splits_fp.exists():
+        with open(splits_fp, 'r', encoding='utf-8') as in_file:
+            splits = json.load(in_file)
+    else:
+        splits = make_splits(
+            original_frames_dir=cfg.paths.original_frames_dir,
+            compressed_frames_dir=cfg.paths.compressed_frames_dir,
+            save_path=splits_fp,
+        )
+    return splits
+
+
+def make_dataset(
+    cfg: Gifnoc,
+    stage: Stage,
+    pipe: Callable[[Path, Path], Any] = identity_pipe,
+) -> ImageFilepathDataset:
+    """Makes an ImageFilepathDataset for a specific stage.
+
+    An ImageFilepathDataset is a custom pytorch dataset that
+    is able to handle image filepath and a custom function
+    called `pipe` to feed a model train/val/test pipeline.
+
+    Available stages: {`train`, `val`, 'test'}
+
+    Args:
+        cfg (Gifnoc): Configuration object
+        stage (str): A string representing the stage,
+            the possible choices are `train`, `val`,
+            and `test`.
+        pipe: (Callable): Custom function to transform
+            image filepaths to something else.
+
+    Returns:
+        ImageFilepathDataset: An ImageFilepathDataset for a specific stage.
+    """
+    splits = get_splits(cfg)
+    original_filepaths = list(
+        itertools.chain.from_iterable(
+            list_files(
+                Path(cfg.paths.original_frames_dir, path), extension='.png'
+            )
+            for path in splits[stage.value]
+        )
+    )
+    compressed_filepaths = list(
+        itertools.chain.from_iterable(
+            list_files(
+                Path(cfg.paths.compressed_frames_dir, path), extension='.jpg'
+            )
+            for path in splits[stage.value]
+        )
+    )
+    return ImageFilepathDataset(
+        original_filepaths=original_filepaths,
+        compressed_filepaths=compressed_filepaths,
+        pipe=pipe,
+    )
+
+
+def make_train_dataloader(cfg) -> DataLoader:
+    """Makes a training dataloader."""
+    dataset = make_dataset(cfg, Stage.TRAIN, default_train_pipe)
     return DataLoader(
         dataset=dataset,
         batch_size=cfg.params.batch_size,
@@ -320,13 +468,9 @@ def make_train_dataloader(cfg: Gifnoc) -> DataLoader:
     )
 
 
-def make_val_dataloader(cfg: Gifnoc) -> DataLoader:
-    dataset = CustomPyTorchDataset(
-        original_frames_dir=cfg.paths.val_original_frames_dir,
-        encoded_frames_dir=cfg.paths.val_encoded_frames_dir,
-        patch_size=cfg.params.patch_size,
-        training=False,
-    )
+def make_val_dataloader(cfg) -> DataLoader:
+    """Makes a validation dataloader."""
+    dataset = make_dataset(cfg, Stage.VAL, default_val_pipe)
     return DataLoader(
         dataset=dataset,
         batch_size=cfg.params.batch_size,
@@ -336,13 +480,9 @@ def make_val_dataloader(cfg: Gifnoc) -> DataLoader:
     )
 
 
-def make_test_dataloader(cfg: Gifnoc) -> DataLoader:
-    dataset = CustomPyTorchDataset(
-        original_frames_dir=cfg.paths.test_original_frames_dir,
-        encoded_frames_dir=cfg.paths.test_encoded_frames_dir,
-        patch_size=cfg.params.patch_size,
-        training=False,
-    )
+def make_test_dataloader(cfg) -> DataLoader:
+    """Makes a testing dataloader."""
+    dataset = make_dataset(cfg, Stage.TEST, default_test_pipe)
     return DataLoader(
         dataset=dataset,
         batch_size=cfg.params.batch_size,
@@ -355,6 +495,7 @@ def make_test_dataloader(cfg: Gifnoc) -> DataLoader:
 def make_dataloaders(
     cfg: Gifnoc,
 ) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """Makes train/val/test dataloaders."""
     return (
         make_train_dataloader(cfg),
         make_val_dataloader(cfg),
