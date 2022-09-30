5e4a27848b253c159bb54c3986cdcd29f26d71a1
lorenzopalloni <palloni.lorenzo@gmail.com>
2022-09-30 07:36:24 +0200

    Update config.py

--- binarization/config.py
+++ binarization/config.py
@@ -30,10 +30,10 @@ class ParamsConfig:
     dis_lr: float = 1e-4
     gen_lr: float = 1e-4
     patch_size: int = 96
-    batch_size: int = 8  # {8, 32}
-    limit_train_batches: Optional[int] = 2
-    limit_val_batches: Optional[int] = 2
-    num_workers: int = 4  # {1, 12}
+    batch_size: int = 16  # {8, 32}
+    limit_train_batches: Optional[int] = None
+    limit_val_batches: Optional[int] = None
+    num_workers: int = 1  # {1, 12}
     num_epochs: int = 100
     w0: float = 1e-0  # LPIPS weight
     w1: float = 1e-0  # SSIM weight
@@ -50,15 +50,8 @@ class PathsConfig:
     artifacts_dir: Path = project_dir / "artifacts"
     mlruns_dir: Path = artifacts_dir / 'mlruns'
     data_dir: Path = project_dir / "data"
-    train_dir: Path = data_dir / "train"
-    val_dir: Path = data_dir / "val"
-    test_dir: Path = data_dir / "test"
-    train_original_frames_dir: Path = train_dir / "original_frames"
-    train_encoded_frames_dir: Path = train_dir / "encoded_frames"
-    val_original_frames_dir: Path = val_dir / "original_frames"
-    val_encoded_frames_dir: Path = val_dir / "encoded_frames"
-    test_original_frames_dir: Path = test_dir / "original_frames"
-    test_encoded_frames_dir: Path = test_dir / "encoded_frames"
+    original_frames_dir: Path = data_dir / "original_frames"
+    compressed_frames_dir: Path = data_dir / "compressed_frames"
 
 
 @dataclass
