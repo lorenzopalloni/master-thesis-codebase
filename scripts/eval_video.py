c624391ea3264096235ff86039a5abcdb672f65c
lorenzopalloni <palloni.lorenzo@gmail.com>
2022-11-28 08:14:42 +0100

    Fix bug in eval_video.py

--- scripts/eval_video.py
+++ scripts/eval_video.py
@@ -32,46 +32,15 @@ def save_with_cv2(tensor_img: torch.Tensor, path: str) -> None:
     cv2.imwrite(path, numpy_img)
 
 
-def write_to_video(pic, writer):
-    pic = inv_min_max_scaler(pic.squeeze(0))
-    npimg = np.transpose(pic.cpu().numpy(), (1, 2, 0)) * 255
-    npimg = npimg.astype('uint8')
-    npimg = cv2.cvtColor(npimg, cv2.COLOR_BGR2RGB)
-
-    font = cv2.FONT_HERSHEY_SIMPLEX
-    cv2.putText(
-        npimg,
-        '540p CRF 23 + bicubic',
-        (50, 1030),
-        font,
-        1,
-        (10, 10, 10),
-        2,
-        cv2.LINE_AA,
-    )
-    cv2.putText(
-        npimg,
-        'SR-Unet (ours)',
-        (1920 // 2 + 50, 1020),
-        font,
-        1,
-        (10, 10, 10),
-        2,
-        cv2.LINE_AA,
-    )
-
-    writer.write(npimg)
-
-
 def cv2_to_torch(numpy_img: npt.NDArray[np.uint8]) -> torch.Tensor:
-    numpy_img = numpy_img / 255
-    torch_img = torch.Tensor(numpy_img).cuda()
-    torch_img = torch.img.permute(2, 0, 1).unsqueeze(0)
-    torch_img = min_max_scaler(torch_img)
-    return torch_img
+    scaled_numpy_img: npt.NDArray[np.float64] = numpy_img / 255
+    tensor_img = torch.Tensor(scaled_numpy_img).cuda()
+    tensor_img = tensor_img.permute(2, 0, 1).unsqueeze(0)
+    tensor_img = min_max_scaler(tensor_img)
+    return tensor_img
 
 
-def torch_to_cv2(tensor_img: torch.Tensor) -> npt.NDArray[np.uint8]:
+def tensor_to_numpy(tensor_img: torch.Tensor) -> npt.NDArray[np.uint8]:
     tensor_img = inv_min_max_scaler(tensor_img.squeeze(0))
     tensor_img = tensor_img.permute(1, 2, 0)
     numpy_img = tensor_img.byte().cpu().numpy()
@@ -79,29 +48,67 @@ def torch_to_cv2(tensor_img: torch.Tensor) -> npt.NDArray[np.uint8]:
     return numpy_img
 
 
-def blend_images(img1: torch.Tensor, img2: torch.Tensor) -> torch.Tensor:
+def blend_images(
+    img1: torch.Tensor,
+    img2: torch.Tensor,
+    shrink: bool = False,
+) -> torch.Tensor:
     assert (
         img1.shape[-1] == img2.shape[-1]
     ), f"{img1.shape=} not equal to {img2.shape=}."
-    width = img1.shape[-1]
-    w_4 = width // 4
-    cropped_i1 = img1[:, :, :, w_4 : w_4 * 3]
-    cropped_i2 = img2[:, :, :, w_4 : w_4 * 3]
-    return torch.cat([cropped_i1, cropped_i2], dim=3)
+    if shrink:
+        width = img1.shape[-1]
+        w_4 = width // 4
+        img1 = img1[:, :, :, w_4 : w_4 * 3]
+        img2 = img2[:, :, :, w_4 : w_4 * 3]
+    return torch.cat([img1, img2], dim=3)
 
 
-def resize_torch_img(
-    torch_img: torch.Tensor, scale_factor: int = 4
+def resize_tensor_img(
+    tensor_img: torch.Tensor, scale_factor: int = 4
 ) -> torch.Tensor:
     return torch.clip(
         F.interpolate(
-            torch_img,
+            tensor_img,
             scale_factor=scale_factor,
             mode='bicubic',
         ),
         min=0,
         max=1,
     )
+
+
+def write_to_video(tensor_img: torch.Tensor, writer: cv2.VideoWriter) -> None:
+    numpy_img = tensor_to_numpy(tensor_img)
+    h, w, _ = numpy_img.shape
+    offset = int(min(w, h) * 0.05)
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+
+    cv2.putText(
+        img=numpy_img,
+        text='bicubic interpolation',
+        # org=(50, 1030),
+        org=(offset, h - offset),
+        fontFace=font,
+        fontScale=0.5,
+        color=(10, 10, 10),
+        thickness=2,
+        lineType=cv2.LINE_AA,
+    )
+    cv2.putText(
+        img=numpy_img,
+        text='Unet (ours)',
+        # org=(1920 // 2 + 50, 1020),
+        org=(w // 2 + offset, h - offset),
+        fontFace=font,
+        fontScale=0.5,
+        color=(10, 10, 10),
+        thickness=2,
+        lineType=cv2.LINE_AA,
+    )
+
+    writer.write(numpy_img)
 
 
 def eval_video(
@@ -115,42 +122,48 @@ def eval_video(
     model = set_up_generator(cfg, device=device)
     reader = torchvision.io.VideoReader(video_path.as_posix(), 'video')
 
-    if enable_write_to_video:
-        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
-        hr_video_writer = cv2.VideoWriter(
-            'rendered.mp4', fourcc, 30, (1920, 1080)
-        )
-
     metadata = reader.get_metadata()
     fps = metadata['video']['fps'][0]
     duration = metadata['video']['duration'][0]
     n_frames = int(fps * duration)
 
-    q0: Queue = Queue(1)
-    q1: Queue = Queue(1)
+    if enable_write_to_video:
+        fourcc = cv2.VideoWriter_fourcc(*'MP4V')
+        hr_video_writer = cv2.VideoWriter(
+            filename='rendered.mp4',
+            fourcc=fourcc,
+            fps=30,
+            # frameSize=(1920, 1080),
+            frameSize=(1024, 384),
+        )
+
+    queue0: Queue = Queue(1)
+    queue1: Queue = Queue(1)
 
     reader.seek(0)
 
-    def read_pic(cap, q: Queue, scale_factor: int = 4):
+    def read_pic(
+        reader: torchvision.io.VideoReader, queue: Queue, scale_factor: int = 4
+    ) -> None:
         while True:
-            img = next(cap)['data']
+            img = next(reader)['data']
             img = min_max_scaler(img)
             img = adjust_image_for_unet(img).unsqueeze(0)
-            resized_img = resize_torch_img(img, scale_factor)
-            q.put((img, resized_img))
+            resized_img = resize_tensor_img(img, scale_factor)
+            queue.put((img, resized_img))
 
-    def show_pic(q):
+    def show_pic(queue):
         while True:
-            torch_img = q.get()
-            img = torch_to_cv2(torch_img)
+            tensor_img = queue.get()
+            img = tensor_to_numpy(tensor_img)
             # img = cv2.resize(img, (64, 64))  # NOTE: remove this /!\
             cv2.imshow('rendering', img)
             cv2.waitKey(1)
 
-    t0 = Thread(target=read_pic, args=(reader, q0, scale_factor))
-    t1 = Thread(target=show_pic, args=(q1,))
-    t0.start()
-    t1.start()
+    thread0 = Thread(target=read_pic, args=(reader, queue0, scale_factor))
+    thread1 = Thread(target=show_pic, args=(queue1,))
+    thread0.start()
+    thread1.start()
 
     model = model.eval()
     with torch.no_grad():
@@ -159,13 +172,15 @@ def eval_video(
 
             tic = time.perf_counter()
 
-            compressed, resized_compressed = q0.get()
-            generated = model(compressed)
+            compressed, resized_compressed = queue0.get()
+            generated = model(compressed).clip(0, 1)
 
             if enable_show_compressed:
-                generated = blend_images(resized_compressed, generated)
+                generated = blend_images(
+                    resized_compressed, generated, shrink=False
+                )
 
-            q1.put(generated)
+            queue1.put(generated)
 
             toc = time.perf_counter()
             elapsed = toc - tic
@@ -175,7 +190,8 @@ def eval_video(
 
             if enable_write_to_video:
                 write_to_video(generated, hr_video_writer)
-                if i == 30 * 10:
+                # if i == 30 * 10:
+                if i == n_frames - 1:
                     hr_video_writer.release()
                     print("Releasing video")
 
@@ -205,5 +221,5 @@ if __name__ == '__main__':
         cfg=default_cfg,
         video_path=homer_video_path,
         enable_show_compressed=True,
-        enable_write_to_video=False,
+        enable_write_to_video=True,
     )
