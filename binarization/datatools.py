Fix normalization, clamp's, rgb to bgr issue

binarization/datatools.py
@@ -64,17 +64,17 @@ def random_crop_images(
     random_width = get_starting_random_position(width, patch_size)
     random_height = get_starting_random_position(height, patch_size)
 
-    compressed_image_positions = (
+    compressed_image_positions = [
         random_width,
         random_height,
         random_width + patch_size,
         random_height + patch_size,
-    )
+    ]
     # scale positions for the original image
-    original_image_positions = (
+    original_image_positions = [
         compressed_position * scale_factor
         for compressed_position in compressed_image_positions
-    )
+    ]
     original_patch = original_image.crop(original_image_positions)
     compressed_patch = compressed_image.crop(compressed_image_positions)
     return original_patch, compressed_patch
@@ -240,31 +240,14 @@ def estimate_n_batches_per_buffer(
     patch_size: int = 96,
 ) -> int:
     """Roughly estimates a good number of batches per buffer."""
-    average_patches_per_image = round((
-        compressed_image_width * compressed_image_height
-    ) / (patch_size**2))  # 56
+    average_patches_per_image = round(
+        (compressed_image_width * compressed_image_height) / (patch_size**2)
+    )  # 56
     average_available_patches = buffer_size * average_patches_per_image
     n_batches_per_buffer = round(
         (average_available_patches / factor) / batch_size
     )
     return n_batches_per_buffer
-
-
-def save_with_cv2(tensor_img: torch.Tensor, path: Path) -> None:
-    """Saves an image with cv2."""
-    tensor_img = inv_min_max_scaler(tensor_img.squeeze(0))
-    numpy_img = np.transpose(tensor_img.cpu().numpy(), (1, 2, 0)) * 255
-    numpy_img = cv2.cvtColor(numpy_img, cv2.COLOR_BGR2RGB)
-    cv2.imwrite(path, numpy_img)
-
-
-def numpy_to_tensor(numpy_img: npt.NDArray[np.uint8]) -> torch.Tensor:
-    """Casts an array from numpy to torch."""
-    scaled_numpy_img: npt.NDArray[np.float64] = numpy_img / 255
-    tensor_img = torch.Tensor(scaled_numpy_img).cuda()
-    tensor_img = tensor_img.permute(2, 0, 1).unsqueeze(0)
-    tensor_img = min_max_scaler(tensor_img)
-    return tensor_img
 
 
 def tensor_to_numpy(tensor_img: torch.Tensor) -> npt.NDArray[np.uint8]:

binarization/models/srunet.py
@@ -20,20 +20,20 @@ class SRUNet(torch.nn.Module):  # pylint: disable=too-many-instance-attributes
     ):
         """SRUNet (Super-Resolution UNet).
 
-            Args:
-                in_channels (int, optional): channel dimension of the input.
-                    Defaults to 3.
-                out_channels (int, optional): channel dimension of the output.
-                    Defaults to 3.
-                num_filters (int, optional): number of filters in the first hidden
-                    layer. Each of the following layers gets twice the number of
-                    filters of its previous layer during encoding phase, and half
-                    the number of filters of its previous layer during decoding
-                    phase. Defaults to 64.
-                use_batch_norm (bool): flag for batch normalization. Defaults to
-                    False.
-                scale_factor (int): scaling factor. Defaults to 4.
-            """
+        Args:
+            in_channels (int, optional): channel dimension of the input.
+                Defaults to 3.
+            out_channels (int, optional): channel dimension of the output.
+                Defaults to 3.
+            num_filters (int, optional): number of filters in the first hidden
+                layer. Each of the following layers gets twice the number of
+                filters of its previous layer during encoding phase, and half
+                the number of filters of its previous layer during decoding
+                phase. Defaults to 64.
+            use_batch_norm (bool): flag for batch normalization. Defaults to
+                False.
+            scale_factor (int): scaling factor. Defaults to 4.
+        """
 
         super().__init__()
 
@@ -89,7 +89,7 @@ class SRUNet(torch.nn.Module):  # pylint: disable=too-many-instance-attributes
         )
         self.up1 = torch.nn.Conv2d(
             in_channels=num_filters // 2,
-            out_channels=(self.scale_factor ** 2) * out_channels,
+            out_channels=(self.scale_factor**2) * out_channels,
             kernel_size=1,
             padding=0,
         )
@@ -128,9 +128,9 @@ class SRUNet(torch.nn.Module):  # pylint: disable=too-many-instance-attributes
         out = self.pixel_shuffle(out)
 
         out += torch.nn.functional.interpolate(
-            batch[:, -self.out_channels :, :, :],
+            batch,
             scale_factor=self.scale_factor,
             mode='bicubic',
         )
 
-        return torch.clamp(out, min=-1, max=1)
+        return torch.clamp(out, min=0, max=1)

binarization/models/unet.py
@@ -130,9 +130,9 @@ class UNet(torch.nn.Module):  # pylint: disable=too-many-instance-attributes
         out = self.pixel_shuffle(out)
 
         out += F.interpolate(
-            batch[:, -self.out_channels :, :, :],  # RGB -> BGR
+            batch,
             scale_factor=float(self.scale_factor),
             mode='bicubic',
         )
 
-        return torch.clamp(out, min=-1, max=1)
+        return torch.clamp(out, min=0, max=1)

binarization/train.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 from pathlib import Path
 
-import lpips
 import mlflow
 import piq
 import torch
@@ -14,11 +13,11 @@ from binarization.config import Gifnoc, get_default_config
 from binarization.dataset import get_train_batches, get_val_batches
 from binarization.models import Discriminator
 from binarization.traintools import (
+    CustomLPIPS,
     set_up_checkpoints_dir,
     set_up_cuda_device,
     set_up_generator,
 )
-from binarization.vaccaro import pytorch_ssim
 
 
 def run_training(cfg: Gifnoc):
@@ -26,7 +25,7 @@ def run_training(cfg: Gifnoc):
 
     checkpoints_dir = set_up_checkpoints_dir(cfg.paths.artifacts_dir)
 
-    device = set_up_cuda_device(device_id=1, verbose=True)
+    device = set_up_cuda_device(device_id=0, verbose=True)
 
     gen = set_up_generator(cfg=cfg, device=device)
     dis = Discriminator()
@@ -35,9 +34,9 @@ def run_training(cfg: Gifnoc):
     gen_optim = torch.optim.Adam(lr=cfg.params.gen_lr, params=gen.parameters())
     dis_optim = torch.optim.Adam(lr=cfg.params.dis_lr, params=dis.parameters())
 
-    ssim_op = pytorch_ssim.SSIM()  # piq.SSIMLoss()
-    lpips_vgg_loss_op = lpips.LPIPS(net='vgg', version='0.1')
-    lpips_alex_metric_op = lpips.LPIPS(net='alex', version='0.1')
+    ssim_loss_op = piq.SSIMLoss()
+    lpips_vgg_loss_op = CustomLPIPS(net='vgg')
+    lpips_alex_metric_op = CustomLPIPS(net='alex')
     bce_loss_op = torch.nn.BCELoss()
 
     lpips_vgg_loss_op.to(device)
@@ -70,7 +69,7 @@ def run_training(cfg: Gifnoc):
 
             original = original.to(device)
             compressed = compressed.to(device)
-            generated = gen(compressed)  # maybe clip it in [0, 1]
+            generated = gen(compressed)
             pred_original = dis(original)
 
             loss_true = bce_loss_op(
@@ -93,7 +92,7 @@ def run_training(cfg: Gifnoc):
             gen_optim.zero_grad()
 
             loss_lpips = lpips_vgg_loss_op(generated, original).mean()
-            loss_ssim = 1.0 - ssim_op(generated, original)
+            loss_ssim = 1.0 - ssim_loss_op(generated, original)
             pred_generated = dis(generated)
             loss_bce = bce_loss_op(
                 pred_generated, torch.ones_like(pred_generated)
@@ -153,14 +152,14 @@ def run_training(cfg: Gifnoc):
             compressed_val = compressed_val.to(device)
             gen.eval()
             with torch.no_grad():
-                generated_val = gen(compressed_val).clip(0, 1)
+                generated_val = gen(compressed_val)
                 metrics_val: dict[str, int | float] = {}
                 metrics_val['lpips_alex_val'] = (
                     lpips_alex_metric_op(generated_val, original_val)
                     .mean()
                     .item()
                 )
-                metrics_val['ssim_val'] = ssim_op(
+                metrics_val['ssim_val'] = ssim_loss_op(
                     generated_val, original_val
                 ).item()
                 metrics_val['psnr_val'] = piq.psnr(
@@ -185,6 +184,7 @@ def run_training(cfg: Gifnoc):
         ##################################################################
         # training_epoch_end - END
     mlflow.log_artifacts(checkpoints_dir)
+
 
 def run_experiment(
     cfg: Gifnoc,
@@ -214,16 +214,17 @@ def run_experiment(
         mlflow.log_params(cfg.model.stringify())
         run_training(cfg)
 
+
 if __name__ == "__main__":
     default_cfg = get_default_config()
-    # default_cfg.params.limit_train_batches = 2
-    # default_cfg.params.limit_val_batches = 2
-    # default_cfg.params.num_epochs = 2
+    default_cfg.params.limit_train_batches = 2
+    default_cfg.params.limit_val_batches = 2
+    default_cfg.params.num_epochs = 2
 
-    unet_ckpt = Path(
-        default_cfg.paths.artifacts_dir,
-        '/checkpoints/2022_09_30_06_31_40/unet_34_106400.pth'
-    )
+    # unet_ckpt = Path(
+    #     default_cfg.paths.artifacts_dir,
+    #     '/checkpoints/2022_09_30_06_31_40/unet_34_106400.pth'
+    # )
     # srunet_ckpt = Path(cfg.paths.artifacts_dir, '/checkpoints/')
 
     # run_experiment(